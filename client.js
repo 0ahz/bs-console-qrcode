e73374f0785199f7cf88d79b0f93d2970f4d5c6d
stephenfri <pengoahz@gmail.com>
2018-09-12 22:44:23 +0800

    bug fix #browser does not display the QR code.

--- client.js
+++ client.js
@@ -5,7 +5,7 @@
   socket.on('connection', function (options) {
     
     console.log('Scan access external url '+options.urls.external);
-    console.log("%c", "padding:100px;line-height:200px;background:url('https://api.qrserver.com/v1/create-qr-code/?size=200x200&format=svg&data="+options.urls.external+"') no-repeat;");
+    console.log("%c ", "padding:100px;background:url('https://api.qrserver.com/v1/create-qr-code/?size=200x200&format=svg&data="+options.urls.external+"') no-repeat;");
 
   });
 
